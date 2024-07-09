d622d56103abf1913412a4e3a53de27199dab931
vakhnenzon <vakhnenzon@mail.de>
2024-07-09 14:42:52 +0200

    Update

---
+++ lesson-2/aws-terraform-lesson-2.tf
@@ -0,0 +1,13 @@
+provider "aws" {}
+
+resource "aws_instance" "my_ubuntu" {
+  count         = 1
+  ami           = "ami-04a81a99f5ec58529"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name    = "My Ubuntu"
+    Owner   = "Vakhnenzon"
+    Project = "Terraform"
+  }
+}
