e2d218166af6277836e9ad06c813b250450cb5fd
vakhnenzon <vakhnenzon@mail.de>
2024-07-10 18:51:14 +0200

    Update

--- lesson-11-alb-launch_template/main.tf
+++ lesson-11-alb-launch_template/main.tf
@@ -34,11 +34,9 @@ data "aws_ami" "latest_amazon_linux" {
 
 #-------------------------------------------------------------------------------
 resource "aws_default_vpc" "default" {}
-
 resource "aws_default_subnet" "default_az1" {
   availability_zone = data.aws_availability_zones.working.names[0]
 }
-
 resource "aws_default_subnet" "default_az2" {
   availability_zone = data.aws_availability_zones.working.names[1]
 }
